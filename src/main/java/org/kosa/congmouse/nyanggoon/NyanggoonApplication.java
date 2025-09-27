9b489242dcec25de7c4f0df934cf470a376e2997
soon-hyeong <shjoo820@naver.com>
2025-09-27 10:57:08 +0900

    init project

---
+++ src/main/java/org/kosa/congmouse/nyanggoon/NyanggoonApplication.java
@@ -0,0 +1,13 @@
+package org.kosa.congmouse.nyanggoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyanggoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyanggoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/kosa/congmouse/nyanggoon/NyanggoonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosa.congmouse.nyanggoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyanggoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
